19a84262b18f5f77aff4390e47ba9c5d0809bfce
emilianocampos <emicampos1268@gmail.com>
2023-11-28 06:51:05 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+let carouselInterval = 3000; // Intervalo en milisegundos (por ejemplo, 3000 para 3 segundos)
+let myCarousel = document.getElementById('carouselExample');
+
+let carousel = new bootstrap.Carousel(myCarousel, {
+  interval: false // Desactiva el intervalo predeterminado de Bootstrap
+});
+
+function avanzarDiapositiva() {
+  carousel.next();
+}
+
+setInterval(avanzarDiapositiva, carouselInterval);

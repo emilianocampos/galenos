19004c010cedbcbb2c03561e7cff80115a12b963
emilianocampos <emicampos1268@gmail.com>
2024-02-23 02:21:48 -0300

    asd

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ AOS.init();
 
 let carouselInterval = 3000; // Intervalo en milisegundos (por ejemplo, 3000 para 3 segundos)
 let myCarousel = document.getElementById('carouselExample');
+let btn = document.getElementById('sub');
+
 
 let carousel = new bootstrap.Carousel(myCarousel, {
   interval: false // Desactiva el intervalo predeterminado de Bootstrap
@@ -12,3 +14,44 @@ function avanzarDiapositiva() {
 }
 
 setInterval(avanzarDiapositiva, carouselInterval);
+
+
+
+//ingreso de datos
+
+function Ingreso(nombre, apellido, num, email, consulta) {
+
+  this.nombre = nombre,
+    this.apellido = apellido,
+    this.num = num,
+    this.email = email,
+    this.consulta = consulta
+
+}
+
+//al tocar el boton sale un sweet a 
+btn.addEventListener('click',() => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+  Toast.fire({
+    icon: "success",
+    title: "Estamos procesando y enviando tu consulta !"
+  });
+
+
+})  
+
+
+
+
+
+
